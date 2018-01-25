48796c8a8f1eef4353578bea5e94e8033641567b
Nagendra <nagendra080389@gmail.com>
2018-01-25 05:54:43 +0530

    code search implemented

--- src/main/resources/static/js/makeEditable.js
+++ src/main/resources/static/js/makeEditable.js
@@ -1,11 +1,15 @@
 var editBtn = document.getElementById('editBtn');
 var saveBtn = document.getElementById('saveBtn');
 var textAreaHtml = document.getElementsByTagName("textarea");
+var globalEditor;
 
 editBtn.addEventListener('click', function (e) {
+    globalEditor = $('.CodeMirror')[0].CodeMirror;
+    globalEditor.readOnly = false;
     textAreaHtml[0].removeAttribute('readonly');
 });
 
 saveBtn.addEventListener('click', function (e) {
+    globalEditor.readOnly = true;
     textAreaHtml[0].setAttribute('readonly', true);
 });
