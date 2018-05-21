cac9ee2e55200c5676af353deaa142bcb4576eb0
nagesingh@deloitte.com <nagesingh@deloitte.com>
2018-05-21 10:29:44 +0530

    UI Changed New Class Creation

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -13,116 +13,219 @@ app.controller('OrderFormController', function($scope, $http) {
         }
         $scope.names = foundClass;
     }).error(function(data) {});
-
     $scope.retrieveSelectedClass = function() {
         if ($scope.selectedName === undefined) {
             return;
         }
-        if ($scope.selectedName === 'New Apex Class....') {
-            return;
-        }
-        var data = {
-            apexClassName: $scope.selectedName
-        };
-        var config = {
-            params: data
-        };
-        $('#loaderImage').show();
-        $http.get("/getApexBody", config).then(function(response) {
-            document.getElementById("saveBtn").disabled = false;
-                if(response.data){
-                $scope.apexClassWrapper = response.data;
-                $('#loaderImage').hide();
-                if (globalEditor1) {
-                    globalEditor1.toTextArea();
-                }
-                setTimeout(function(test) {
-                    var ExcludedIntelliSenseTriggerKeys = {
-                        "8": "backspace",
-                        "9": "tab",
-                        "13": "enter",
-                        "16": "shift",
-                        "17": "ctrl",
-                        "18": "alt",
-                        "19": "pause",
-                        "20": "capslock",
-                        "27": "escape",
-                        "33": "pageup",
-                        "34": "pagedown",
-                        "35": "end",
-                        "36": "home",
-                        "37": "left",
-                        "38": "up",
-                        "39": "right",
-                        "40": "down",
-                        "45": "insert",
-                        "46": "delete",
-                        "90": "ctrl-z",
-                        "91": "left window key",
-                        "92": "right window key",
-                        "93": "select",
-                        "107": "add",
-                        "109": "subtract",
-                        "110": "decimal point",
-                        "111": "divide",
-                        "112": "f1",
-                        "113": "f2",
-                        "114": "f3",
-                        "115": "f4",
-                        "116": "f5",
-                        "117": "f6",
-                        "118": "f7",
-                        "119": "f8",
-                        "120": "f9",
-                        "121": "f10",
-                        "122": "f11",
-                        "123": "f12",
-                        "144": "numlock",
-                        "145": "scrolllock",
-                        "186": "semicolon",
-                        "187": "equalsign",
-                        "188": "comma",
-                        "189": "dash",
-                        "190": "period",
-                        "191": "slash",
-                        "192": "graveaccent",
-                        "220": "backslash",
-                        "222": "quote"
+        if ($scope.selectedName.groupName === 'Create New') {
+            bootbox.prompt({
+                title: 'Enter Class Name',
+                placeholder: 'Enter Class Name',
+                buttons: {
+                    confirm: {
+                        label: 'Create'
                     }
-                    var editor = CodeMirror.fromTextArea(document.getElementById('apexBody'), {
-                        lineNumbers: true,
-                        matchBrackets: true,
-                        styleActiveLine: true,
-                        extraKeys: {
-                            ".": function(editor) {
-                                setTimeout(function() {
-                                    editor.execCommand("autocomplete");
-                                }, 100);
-                                throw CodeMirror.Pass; // tell CodeMirror we didn't handle the key
+                },
+                callback: function(value) {
+                    if (value == null) {
+                        return;
+                    }
+                    $http.post("/createFile", value).success(function(data) {
+                        if (data.data) {
+                            $scope.apexClassWrapper = data;
+                            $('#loaderImage').hide();
+                            if (globalEditor1) {
+                                globalEditor1.toTextArea();
                             }
-                        },
-                        gutters: ["CodeMirror-lint-markers"],
-                        lint: true,
-                        mode: "text/x-apex"
-                    });
-                    editor.on("keyup", function(cm, event) {
-                        var keyCode = event.keyCode || event.which;
-                        if (!ExcludedIntelliSenseTriggerKeys[(event.keyCode || event.which).toString()]) {
-                            if (timeout) clearTimeout(timeout);
-                            timeout = setTimeout(function() {
-                                editor.showHint({
-                                    hint: CodeMirror.hint.auto,
-                                    completeSingle: false
+                            setTimeout(function(test) {
+                                var ExcludedIntelliSenseTriggerKeys = {
+                                    "8": "backspace",
+                                    "9": "tab",
+                                    "13": "enter",
+                                    "16": "shift",
+                                    "17": "ctrl",
+                                    "18": "alt",
+                                    "19": "pause",
+                                    "20": "capslock",
+                                    "27": "escape",
+                                    "33": "pageup",
+                                    "34": "pagedown",
+                                    "35": "end",
+                                    "36": "home",
+                                    "37": "left",
+                                    "38": "up",
+                                    "39": "right",
+                                    "40": "down",
+                                    "45": "insert",
+                                    "46": "delete",
+                                    "90": "ctrl-z",
+                                    "91": "left window key",
+                                    "92": "right window key",
+                                    "93": "select",
+                                    "107": "add",
+                                    "109": "subtract",
+                                    "110": "decimal point",
+                                    "111": "divide",
+                                    "112": "f1",
+                                    "113": "f2",
+                                    "114": "f3",
+                                    "115": "f4",
+                                    "116": "f5",
+                                    "117": "f6",
+                                    "118": "f7",
+                                    "119": "f8",
+                                    "120": "f9",
+                                    "121": "f10",
+                                    "122": "f11",
+                                    "123": "f12",
+                                    "144": "numlock",
+                                    "145": "scrolllock",
+                                    "186": "semicolon",
+                                    "187": "equalsign",
+                                    "188": "comma",
+                                    "189": "dash",
+                                    "190": "period",
+                                    "191": "slash",
+                                    "192": "graveaccent",
+                                    "220": "backslash",
+                                    "222": "quote"
+                                }
+                                var editor = CodeMirror.fromTextArea(document.getElementById('apexBody'), {
+                                    lineNumbers: true,
+                                    matchBrackets: true,
+                                    styleActiveLine: true,
+                                    extraKeys: {
+                                        ".": function(editor) {
+                                            setTimeout(function() {
+                                                editor.execCommand("autocomplete");
+                                            }, 100);
+                                            throw CodeMirror.Pass; // tell CodeMirror we didn't handle the key
+                                        }
+                                    },
+                                    gutters: ["CodeMirror-lint-markers"],
+                                    lint: true,
+                                    mode: "text/x-apex"
                                 });
-                            }, 150);
+                                editor.on("keyup", function(cm, event) {
+                                    var keyCode = event.keyCode || event.which;
+                                    if (!ExcludedIntelliSenseTriggerKeys[(event.keyCode || event.which).toString()]) {
+                                        if (timeout) clearTimeout(timeout);
+                                        timeout = setTimeout(function() {
+                                            editor.showHint({
+                                                hint: CodeMirror.hint.auto,
+                                                completeSingle: false
+                                            });
+                                        }, 150);
+                                    }
+                                });
+                                globalEditor1 = $('.CodeMirror')[0].CodeMirror;
+                            }), 2000
                         }
-                    });
-                    globalEditor1 = $('.CodeMirror')[0].CodeMirror;
-                }), 2000
-            }
+                    }).error(function(data) {});
+                }
+            });
+        } else {
+            var data = {
+                apexClassName: $scope.selectedName
+            };
+            var config = {
+                params: data
+            };
+            $('#loaderImage').show();
+            $http.get("/getApexBody", config).then(function(response) {
+                if (response.data) {
+                    $scope.apexClassWrapper = response.data;
+                    $('#loaderImage').hide();
+                    if (globalEditor1) {
+                        globalEditor1.toTextArea();
+                    }
+                    setTimeout(function(test) {
+                        var ExcludedIntelliSenseTriggerKeys = {
+                            "8": "backspace",
+                            "9": "tab",
+                            "13": "enter",
+                            "16": "shift",
+                            "17": "ctrl",
+                            "18": "alt",
+                            "19": "pause",
+                            "20": "capslock",
+                            "27": "escape",
+                            "33": "pageup",
+                            "34": "pagedown",
+                            "35": "end",
+                            "36": "home",
+                            "37": "left",
+                            "38": "up",
+                            "39": "right",
+                            "40": "down",
+                            "45": "insert",
+                            "46": "delete",
+                            "90": "ctrl-z",
+                            "91": "left window key",
+                            "92": "right window key",
+                            "93": "select",
+                            "107": "add",
+                            "109": "subtract",
+                            "110": "decimal point",
+                            "111": "divide",
+                            "112": "f1",
+                            "113": "f2",
+                            "114": "f3",
+                            "115": "f4",
+                            "116": "f5",
+                            "117": "f6",
+                            "118": "f7",
+                            "119": "f8",
+                            "120": "f9",
+                            "121": "f10",
+                            "122": "f11",
+                            "123": "f12",
+                            "144": "numlock",
+                            "145": "scrolllock",
+                            "186": "semicolon",
+                            "187": "equalsign",
+                            "188": "comma",
+                            "189": "dash",
+                            "190": "period",
+                            "191": "slash",
+                            "192": "graveaccent",
+                            "220": "backslash",
+                            "222": "quote"
+                        }
+                        var editor = CodeMirror.fromTextArea(document.getElementById('apexBody'), {
+                            lineNumbers: true,
+                            matchBrackets: true,
+                            styleActiveLine: true,
+                            extraKeys: {
+                                ".": function(editor) {
+                                    setTimeout(function() {
+                                        editor.execCommand("autocomplete");
+                                    }, 100);
+                                    throw CodeMirror.Pass; // tell CodeMirror we didn't handle the key
+                                }
+                            },
+                            gutters: ["CodeMirror-lint-markers"],
+                            lint: true,
+                            mode: "text/x-apex"
+                        });
+                        editor.on("keyup", function(cm, event) {
+                            var keyCode = event.keyCode || event.which;
+                            if (!ExcludedIntelliSenseTriggerKeys[(event.keyCode || event.which).toString()]) {
+                                if (timeout) clearTimeout(timeout);
+                                timeout = setTimeout(function() {
+                                    editor.showHint({
+                                        hint: CodeMirror.hint.auto,
+                                        completeSingle: false
+                                    });
+                                }, 150);
+                            }
+                        });
+                        globalEditor1 = $('.CodeMirror')[0].CodeMirror;
+                    }), 2000
+                }
             });
         }
-
+    }
     $scope.postdata = function(apexClassWrapper) {
         console.log(apexClassWrapper);
         apexClassWrapper.body = globalEditor1.getValue();
@@ -179,7 +282,6 @@ app.controller('OrderFormController', function($scope, $http) {
             $('#error').html(data.message);
         });
     };
-
     $scope.deployWithErrors = function(apexClassWrapper) {
         $('#myModal').modal('hide');
         $('#myModalWithoutError').modal('hide');
@@ -252,25 +354,9 @@ app.controller('OrderFormController', function($scope, $http) {
         var cleaneddata = globalEditor1.getValue().replace(new RegExp(' +', 'g'), ' ');
         globalEditor1.getDoc().setValue(cleaneddata);
     };
-    $scope.newClassCreation = function() {
-        bootbox.prompt({
-            title: 'Enter Class Name',
-            placeholder: 'Enter Class Name',
-            buttons: {
-                confirm: {
-                    label: 'Create'
-                }
-            },
-            callback: function(value) {
-                if (value == null) {
-                    return;
-                }
-                $http.post("/createFile", value).success(function(data) {
-                    $scope.newApexClassWrapper = data;
-                }).error(function(data) {});
-            }
-        });
-    };
+    /*$scope.newClassCreation = function() {
+
+    };*/
 });
 
 function testAnim(x) {
