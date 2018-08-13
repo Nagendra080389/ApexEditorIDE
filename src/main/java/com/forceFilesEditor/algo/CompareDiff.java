3d5d17069fc72a00fbf2fcd346f10796f98e5905
nagesingh@deloitte.com <nagesingh@deloitte.com>
2018-08-13 10:24:39 +0530

    remove unused imports

--- src/main/java/com/forceFilesEditor/algo/CompareDiff.java
+++
@@ -1,8 +0,0 @@
-package com.forceFilesEditor.algo;
-
-public class CompareDiff {
-
-    public static boolean checkForEquals(String bodyFromOrg, String backUpBodyFromOrg){
-        return bodyFromOrg.equals(backUpBodyFromOrg);
-    }
-}
